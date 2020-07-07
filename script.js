cfd2881d78be7eeb2e33725d6ba626d9995b36b1
Barrett Pope <barrettpope94@gmail.com>
2020-07-07 11:29:35 -0400

    select variables added to JS file. Content and styling added to HTML and CSS files.

---
+++ script.js
@@ -0,0 +1,5 @@
+var buttonA = document.querySelector("#btnA");
+var buttonB = document.querySelector("#btnB");
+var buttonC = document.querySelector("#btnC");
+var buttonD = document.querySelector("#btnD");
+
